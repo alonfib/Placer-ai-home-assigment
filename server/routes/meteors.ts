b6d8dfb5c0c41abf772a5044dced905448da0f55
Alon Fibert <alonfib@gmail.com>
2023-10-02 16:02:53 +0300

    meteor routes & types

--- server/routes/meteors.ts
+++ server/routes/meteors.ts
@@ -1,11 +1,29 @@
-var express = require('express');
+const express = require('express');
 const { Meteors } = require('../db/collections/meteors');
-var router = express.Router();
+const { IMeteor, MeteorsResponse } = require('../types');
 
-/* GET users listing. */
-// TODO: add pagination.
-router.get('/meteors', function(req, res, next) {
-  res.send(Meteors);
+const DEFAULT_PAGE = 1;
+const DEFAULT_PER_PAGE = 10;
+
+const router = express.Router();
+
+/* GET meteors listing with pagination. */
+router.get('/meteors', (req, res) => {
+  const page = parseInt(req.query.page) || DEFAULT_PAGE;
+  const perPage = parseInt(req.query.perPage) || DEFAULT_PER_PAGE;
+
+  const startIndex = (page - 1) * perPage;
+  const endIndex = page * perPage;
+
+  const meteorsSubset = Meteors.slice(startIndex, endIndex);
+
+  const response = {
+    meteors: meteorsSubset,
+    currentPage: page,
+    totalPages: Math.ceil(Meteors.length / perPage) || 1,
+  };
+
+  res.json(response);
 });
 
 module.exports = router;
--- server/types.ts
+++ server/types.ts
@@ -1,3 +1,16 @@
 import { Meteors } from "./db/collections/meteors";
 
 export type IMeteor = typeof Meteors[number];
+
+export interface MeteorsRequest {
+  page?: number;
+  perPage?: number;
+  year?: number;
+  mass?: number;
+}
+
+export interface MeteorsResponse {
+  meteors: IMeteor[];
+  currentPage: number;
+  totalPages: number;
+}
