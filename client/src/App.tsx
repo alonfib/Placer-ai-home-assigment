61baa781acaddc81c4b4b8295fe7a810d3dbf995
Alon Fibert <alonfib@gmail.com>
2023-10-02 15:16:12 +0300

    init app request & data visible on screen

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,42 @@
-import React from 'react';
-import './App.css';
-import { get } from './api';
+import React, { useEffect } from "react";
+import "./App.scss";
+import { get } from "./api";
+import { IMeteor } from "../../server/types";
 
 function App() {
+  const [meteorsData, setMeteorsData] = React.useState<IMeteor[]>([]);
+  console.log("meteorsData", meteorsData)
 
-  const onClick = async () => {
-    const data = await get('');
-  }
+  const fetchData = async () => {
+    const data: IMeteor[] = await get("");
+    setMeteorsData(data ?? []);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <div className="App">
-      <button onClick={onClick}> send api request </button>
+      <div className="headers">
+        <div className="header">name</div>
+        <div className="header">recClass</div>
+        <div className="header">mass</div>
+        <div className="header">year</div>
+        <input></input>
+      </div>
+      <div className="content">
+        {meteorsData.length ? meteorsData?.map((meteor) => {
+          return (
+            <div className="row">
+              <div className="row-data">{meteor.name}</div>
+              <div className="row-data">{meteor.recClass}</div>
+              <div className="row-data">{meteor?.mass  ?? "Unknown"}</div>
+              <div className="row-data">{meteor.year}</div>
+            </div>
+          );
+        }) : <div> No data </div>}
+      </div>
     </div>
   );
 }
